c7b196273dc885e678c459ab72669f9557ba7c16
romesrmj <romesrmj@gmail.com>
2023-04-17 15:15:07 -0300

    Update zabbix_auto.sh

--- zabbix_auto.sh
+++ zabbix_auto.sh
@@ -1,57 +1,66 @@
 #!/bin/bash
 
-# Define a timezone para São Paulo
-echo "Configurando timezone..."
-sudo timedatectl set-timezone America/Sao_Paulo
+PASSWORD="zbx#2k23$aut0"
 
-# Atualiza o sistema
-echo "Atualizando o sistema..."
-sudo apt update -y && sudo apt upgrade -y
+# Definir a senha do root do MySQL
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
 
-# Instala o Apache, PHP, MySQL e outras ferramentas necessárias
-echo "Instalando Apache, PHP, MySQL e outras ferramentas necessárias..."
-sudo apt install -y apache2 php libapache2-mod-php mysql-server mysql-client php-mysql php-gd php-xml php-bcmath php-mbstring php-ldap php-xmlrpc php-soap net-tools snmp
+# Instalar os pacotes necessários
+apt-get update
+apt-get install -y apache2 mysql-server mysql-client snmp snmpd libmysqlclient-dev libsnmp-dev libopenipmi-dev libcurl4-gnutls-dev fping libxml2-dev libevent-dev make automake libtool libpcre3-dev libssl-dev libsnmp-dev libcurl4-openssl-dev pkg-config php php-cgi libapache2-mod-php php-common php-pear php-mbstring php-gd php-intl php-mysql php-bcmath php-zip php-xml php-ldap composer
 
-# Altera a senha do root do MySQL
-echo "Alterando a senha do root do MySQL..."
-sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'zbx#2k23$aut0'; FLUSH PRIVILEGES;"
+# Definir timezone para São Paulo
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+dpkg-reconfigure -f noninteractive tzdata
 
-# Adiciona o repositório do Zabbix
-echo "Adicionando o repositório do Zabbix..."
-wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu22.04_all.deb
-sudo dpkg -i zabbix-release_6.4-1+ubuntu22.04_all.deb
+# Adicionar o repositório do Zabbix
+wget https://repo.zabbix.com/zabbix/6.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.2-1+ubuntu22.04_all.deb
+dpkg -i zabbix-release_6.2-1+ubuntu22.04_all.deb
+apt-get update
 
-# Instala o Zabbix Server e o Frontend
-echo "Instalando o Zabbix Server e o Frontend..."
-sudo apt update -y && sudo apt install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-agent
+# Instalar o Zabbix server e frontend
+apt-get install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf
 
-# Importa o banco de dados inicial do Zabbix
-echo "Importando o banco de dados inicial do Zabbix..."
-sudo mysql -uroot -pzbx#2k23$aut0 zabbix < /usr/share/doc/zabbix-server-mysql*/create.sql.gz
+# Define o idioma padrão do Zabbix para pt-BR
+sudo sed -i "s/'language' => 'en_GB'/'language' => 'pt_BR'/g" /usr/share/zabbix/include/classes/core/CWebUser.php
 
-# Configura o Zabbix Server
-echo "Configurando o Zabbix Server..."
-sudo sed -i "s/^DBPassword=.*/DBPassword=zbx#2k23$aut0/" /etc/zabbix/zabbix_server.conf
-sudo systemctl restart zabbix-server zabbix-agent apache2
+# Criar banco de dados para o Zabbix
+mysql -uroot -p$PASSWORD -e "CREATE DATABASE zabbix CHARACTER SET utf8 COLLATE utf8_bin;"
+mysql -uroot -p$PASSWORD -e "CREATE USER 'zabbix'@'localhost' IDENTIFIED BY '$PASSWORD';"
+mysql -uroot -p$PASSWORD -e "GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'localhost';"
 
-# Adiciona o repositório do Grafana
-echo "Adicionando o repositório do Grafana..."
+# Importar o schema do banco de dados do Zabbix
+cd /usr/share/doc/zabbix-server-mysql
+gunzip -c create.sql.gz | mysql -uzabbix -p$PASSWORD zabbix
+
+# Habilitar e iniciar o serviço do Zabbix server
+systemctl enable zabbix-server
+systemctl start zabbix-server
+
+# Configurar o firewall
+ufw allow 80/tcp
+ufw allow 443/tcp
+ufw allow 10050/tcp
+ufw allow 10051/tcp
+ufw enable
+
+# Instalar o Grafana
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
+sudo apt-get update
+sudo apt-get install -y grafana
 
-# Instala o Grafana
-echo "Instalando o Grafana..."
-sudo apt update -y && sudo apt install -y grafana
-
-# Configura o Grafana
-echo "Configurando o Grafana..."
-sudo systemctl daemon-reload
-sudo systemctl start grafana-server
+# Habilitar e iniciar o serviço do Grafana
 sudo systemctl enable grafana-server
+sudo systemctl start grafana-server
 
-# Configura o Zabbix Plugin para Grafana
-echo "Configurando o Zabbix Plugin para Grafana..."
-sudo grafana-cli plugins install alexanderzobnin-zabbix-app
-sudo systemctl restart grafana-server
+# Instalar o SNMP
+apt-get install -y snmp snmpd net-snmp snmp-mibs-downloader
+
+# Reiniciar o Apache
+systemctl restart apache2
+
+echo
 
 echo "Script finalizado!"
