29b73ef66c870efeca073698775d39fb57b8399f
romesrmj <romesrmj@gmail.com>
2023-04-17 20:56:06 -0300

    Update zabbix_auto.sh

--- zabbix_auto.sh
+++ zabbix_auto.sh
@@ -58,7 +58,7 @@ apt install zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-age
 
 #CRIANDO BANCO DE DADOS
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "create database zabbix character set utf8mb4 collate utf8mb4_bin;"
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "create user zabbix@localhost identified by 'password';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "create user zabbix@localhost identified by '$ZABBIX_ADMIN_PASSWORD';"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "grant all privileges on zabbix.* to zabbix@localhost;"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "set global log_bin_trust_function_creators = 1;"
 quit;
