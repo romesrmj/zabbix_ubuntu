93abd7e6b6e7b6c6dfa48f6565fbe01fc92ac78a
romesrmj <romesrmj@gmail.com>
2023-04-17 14:07:18 -0300

    Update zabbix_auto.sh

--- zabbix_auto.sh
+++ zabbix_auto.sh
@@ -1,48 +1,57 @@
 #!/bin/bash
 
+# Define a timezone para São Paulo
+echo "Configurando timezone..."
+sudo timedatectl set-timezone America/Sao_Paulo
+
 # Atualiza o sistema
-sudo apt update && sudo apt upgrade -y
+echo "Atualizando o sistema..."
+sudo apt update -y && sudo apt upgrade -y
 
-# Instala os pacotes necessários para o Zabbix
-sudo apt install -y apache2 mysql-server php php-mysql php-gd php-xml php-bcmath php-mbstring php-ldap php-xmlrpc libapache2-mod-php
+# Instala o Apache, PHP, MySQL e outras ferramentas necessárias
+echo "Instalando Apache, PHP, MySQL e outras ferramentas necessárias..."
+sudo apt install -y apache2 php libapache2-mod-php mysql-server mysql-client php-mysql php-gd php-xml php-bcmath php-mbstring php-ldap php-xmlrpc php-soap net-tools snmp
 
-# Cria o banco de dados do Zabbix
-sudo mysql -u root -p -e "CREATE DATABASE zabbixdb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-sudo mysql -u root -p -e "CREATE USER 'zabbixuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'zbx#2k23$aut0';"
-sudo mysql -u root -p -e "GRANT ALL PRIVILEGES ON zabbixdb.* TO 'zabbixuser'@'localhost';"
-sudo mysql -u root -p -e "FLUSH PRIVILEGES;"
+# Altera a senha do root do MySQL
+echo "Alterando a senha do root do MySQL..."
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'zbx#2k23$aut0'; FLUSH PRIVILEGES;"
 
-# Baixa e instala o pacote do Zabbix
-wget https://repo.zabbix.com/zabbix/5.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.4-1+ubuntu22.04_all.deb
-sudo dpkg -i zabbix-release_5.4-1+ubuntu22.04_all.deb
-sudo apt update
+# Adiciona o repositório do Zabbix
+echo "Adicionando o repositório do Zabbix..."
+wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu22.04_all.deb
+sudo dpkg -i zabbix-release_6.4-1+ubuntu22.04_all.deb
 
-# Instala o Zabbix Server, Zabbix Frontend e Zabbix Agent
-sudo apt install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent
+# Instala o Zabbix Server e o Frontend
+echo "Instalando o Zabbix Server e o Frontend..."
+sudo apt update -y && sudo apt install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-agent
 
-# Edita o arquivo de configuração do Zabbix Server
-sudo sed -i "s/^DBName=.*$/DBName=zabbixdb/" /etc/zabbix/zabbix_server.conf
-sudo sed -i "s/^DBUser=.*$/DBUser=zabbixuser/" /etc/zabbix/zabbix_server.conf
-sudo sed -i "s/^DBPassword=.*$/DBPassword=zbx#2k23$aut0/" /etc/zabbix/zabbix_server.conf
+# Importa o banco de dados inicial do Zabbix
+echo "Importando o banco de dados inicial do Zabbix..."
+sudo mysql -uroot -pzbx#2k23$aut0 zabbix < /usr/share/doc/zabbix-server-mysql*/create.sql.gz
 
-# Reinicia o serviço do Zabbix Server e ativa-o para inicialização automática
+# Configura o Zabbix Server
+echo "Configurando o Zabbix Server..."
+sudo sed -i "s/^DBPassword=.*/DBPassword=zbx#2k23$aut0/" /etc/zabbix/zabbix_server.conf
 sudo systemctl restart zabbix-server zabbix-agent apache2
-sudo systemctl enable zabbix-server zabbix-agent apache2
 
 # Adiciona o repositório do Grafana
+echo "Adicionando o repositório do Grafana..."
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
 
 # Instala o Grafana
-sudo apt update
-sudo apt install -y grafana
+echo "Instalando o Grafana..."
+sudo apt update -y && sudo apt install -y grafana
 
-# Configura o Grafana para se comunicar com o Zabbix
+# Configura o Grafana
+echo "Configurando o Grafana..."
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server
+
+# Configura o Zabbix Plugin para Grafana
+echo "Configurando o Zabbix Plugin para Grafana..."
 sudo grafana-cli plugins install alexanderzobnin-zabbix-app
-sudo grafana-cli admin reset-admin-password zbx#2k23$aut0
 sudo systemctl restart grafana-server
 
-# Abre o firewall para o Grafana
-sudo ufw allow 3000/tcp
-
-# Fim do script
+echo "Script finalizado!"
