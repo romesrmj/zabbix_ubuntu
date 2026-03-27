c1cd9c5739ad4e5a5ee9b96437e7e44fe332b8a3
romesrmj <romesrmj@gmail.com>
2026-03-26 23:53:44 -0300

    Update roolback_zbx7.sh

--- roolback_zbx7.sh
+++ roolback_zbx7.sh
@@ -20,10 +20,8 @@ trap 'echo "[ERRO] Falha na linha ${LINENO}. Saindo." >&2; exit 1' ERR
 # Configuracao
 # ==============================================================================
 readonly LOG_FILE="/var/log/zabbix_grafana_rollback.log"
-readonly GRAFANA_PLUGINS=(
-  "alexanderzobnin-zabbix-app"
-  "marcusolsson-dynamictext-panel"
-)
+readonly GRAFANA_PLUGINS="alexanderzobnin-zabbix-app"
+  
 FORCE=false
 
 # ==============================================================================
